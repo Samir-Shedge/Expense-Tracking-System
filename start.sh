066e5ef7f2c3f6558def1b2d98b3245b0bf4667f
Samir-Shedge <samirshedge4153@gmail.com>
2025-07-19 15:38:07 +0530

    update the files

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # uvicorn server:app --host 0.0.0.0 --port 10000
-uvicorn backend.server:app --reload
+# uvicorn backend.server:app --reload
+#!/bin/bash
+uvicorn backend.server:app --host 0.0.0.0 --port 8000 --reload
